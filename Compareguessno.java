ed4c6e27db0a0e226e79c28d5019fc48672dd015
Rajani2710 <144085935+Rajani2710@users.noreply.github.com>
2024-07-24 15:05:02 +0530

    Update Compareguessno.java

--- Compareguessno.java
+++ Compareguessno.java
@@ -6,45 +6,30 @@ package com;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Compareguessno {
-	public static void main(String[] args) {
-		
-		int minNum=1, maxNum=100;
-		
-		Random random=new Random();
-		
-        int randomNumber = random.nextInt(maxNum - minNum + 1) + minNum;
-		
-		Scanner sc=new Scanner(System.in);
-		
-		System.out.println("Guess the no  between"+ minNum+ "and" +maxNum+":");
-		
-		int userguess =sc.nextInt();
-		
-		if (userguess == randomNumber)
-		{
-			System.out.println("Congratulations! Your guess is correct");
-			
-		}
-		
-		
-		else if(userguess < minNum || userguess> maxNum)
-		{
-			System.out.println("Please enter a number within the specified range.");
-		}
-		
-		else if(userguess < randomNumber)
-		{
-			System.out.println("Too low. Try again!");
-		}
-		
-		else
-		{
-			System.out.println("Too high. Try again!");
-		}
-		
-		
-		sc.close();
-	}
+public class Compareguessnumber {
+    public static void main(String[] args) {
+        int minNum = 1;
+        int maxNum = 100;
 
+        Random random = new Random();
+        int randomNumber = random.nextInt(maxNum - minNum + 1) + minNum;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Guess the number between " + minNum + " and " + maxNum + ":");
+
+        int userGuess = sc.nextInt();
+
+        if (userGuess == randomNumber) {
+            System.out.println("Congratulations! Your guess is correct.");
+        } else if (userGuess < minNum || userGuess > maxNum) {
+            System.out.println("Please enter a number within the specified range.");
+        } else if (userGuess < randomNumber) {
+            System.out.println("Too low. Try again!");
+        } else {
+            System.out.println("Too high. Try again!");
+        }
+
+        sc.close();
+    }
 }
