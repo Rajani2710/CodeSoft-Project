ee0d494be9f9a0067dac784ed593016b55915318
Rajani2710 <144085935+Rajani2710@users.noreply.github.com>
2024-07-24 15:13:30 +0530

    Update Guesstheno.java

--- Guesstheno.java
+++ Guesstheno.java
@@ -5,30 +5,28 @@ package com;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Guesstheno {
-	public static void main(String[]args)
-	{
-		int minNum=1 ,maxNum=10;
-		
-		Random random = new Random();
-		
-		int randomNumber = random.nextInt(maxNum - minNum + 1) + minNum;		
-		Scanner sc=new Scanner(System.in);
-		
-		System.out.println("Guess the Num between"+minNum+"and"+maxNum+":");
-		
-		int UserGuess=sc.nextInt();
-		
-		if(UserGuess == randomNumber)
-		{
-			System.out.println("Congratulations!  Your guess is correct");
-		}
-		else
-		{
-			System.out.println("Sorry the correct number was:"+ randomNumber);
-		}
-		
-		sc.close();
-	}
+public class Guessthenumber {
+    public static void main(String[] args) {
+        int minNum = 1, maxNum = 10;
 
+        Random random = new Random();
+        int randomNumber = random.nextInt(maxNum - minNum + 1) + minNum;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Guess the number between " + minNum + " and " + maxNum + ":");
+        
+        // Prompt user to enter their guess
+        System.out.print("Enter your guess: ");
+        int userGuess = sc.nextInt();
+
+        // Compare the user's guess with the generated number
+        if (userGuess == randomNumber) {
+            System.out.println("Congratulations! Your guess is correct.");
+        } else {
+            System.out.println("Sorry, the correct number was: " + randomNumber);
+        }
+
+        sc.close();
+    }
 }
