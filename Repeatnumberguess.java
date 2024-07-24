b1049df552d450ca12154fe2568ccb3328e19033
Rajani2710 <144085935+Rajani2710@users.noreply.github.com>
2024-07-24 15:19:55 +0530

    Update Repeatnumberguess.java

--- Repeatnumberguess.java
+++ Repeatnumberguess.java
@@ -5,50 +5,46 @@ package com;
 
 import java.util.Scanner;
 
-public class Repeatnumguess {
-	
-	 public static void main(String[] args) {
-	        Scanner sc = new Scanner(System.in);
-	        boolean playAgain = true;
+public class RepeatnumGuess {
 
-	        while (playAgain) {
-	            if (playGame(sc)) {
-	                
-	                String input = sc.nextLine().toLowerCase();
-	                playAgain = input.equals("yes");
-	            } else {
-	                playAgain = false;
-	            }
-	        }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean playAgain = true;
 
-	        System.out.println("Thanks for playing!");
-	        sc.close();
-	    }
+        while (playAgain) {
+            playGame(sc);
 
-	    public static boolean playGame(Scanner sc) {
-	        
-	        int randomNumber = (int) (Math.random() * 15) + 1;
-	        int guess;
-	        int attempts = 0;
+            System.out.print("Do you want to play again? (yes/no): ");
+            String input = sc.next().toLowerCase();
+            playAgain = input.equals("yes");
+        }
 
-	        System.out.println("Guess the number between 1 and 15!");
+        System.out.println("Thanks for playing!");
+        sc.close();
+    }
 
-	        do {
-	            System.out.print("Enter your guess: ");
-	            guess = sc.nextInt();
-	            attempts++;
+    public static void playGame(Scanner sc) {
+        int randomNumber = (int) (Math.random() * 15) + 1;
+        int guess;
+        int attempts = 0;
 
-	            if (guess < randomNumber) {
-	                System.out.println("Too low! Try again.");
-	            } else if (guess > randomNumber) {
-	                System.out.println("Too high! Try again.");
-	            } else {
-	                System.out.println("Congratulations! You guessed it right in " + attempts + " attempts!");
-	                return true;
-	            }
-	        } while (guess != randomNumber);
+        System.out.println("Guess the number between 1 and 15!");
 
-	        sc.nextLine();
-	        return false; 
-	    }
-	}
+        do {
+            System.out.print("Enter your guess: ");
+            guess = sc.nextInt();
+            attempts++;
+
+            if (guess < randomNumber) {
+                System.out.println("Too low! Try again.");
+            } else if (guess > randomNumber) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("Congratulations! You guessed it right in " + attempts + " attempts!");
+            }
+        } while (guess != randomNumber);
+
+        // Consume the newline character left by nextInt()
+        sc.nextLine();
+    }
+}
