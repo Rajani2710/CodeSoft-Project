89bd3791fd64c77771873b25902e40c9a31a66e4
Rajani2710 <144085935+Rajani2710@users.noreply.github.com>
2024-07-24 15:01:11 +0530

    Update Numberguess.java

--- Numberguess.java
+++ Numberguess.java
@@ -6,53 +6,34 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Numberguess {
-	public static void main(String []args)
-	{
-		
-		int minNum=1;
-		int maxNum=100;
-		
-		Random random=new Random();
-		Scanner sc=new Scanner(System.in);
-		
-		int randomNum=random.nextInt(maxNum - minNum +1)+minNum;
-		
-		int UserGuess;
-		int attempts=0;
-		
-		System.out.println("Welcome to the Number Guessing Game!");
-		System.out.println("Guess a number between " + minNum + " and " + maxNum);
-		
-		do
-		{
-			System.out.println("Enter Your Guess=");
-			
-			UserGuess=sc.nextInt();
-			attempts++;
-			
-			if(UserGuess < randomNum)
-			{
-				System.out.println("To Low.  Try Again");
-			}
-			else if (UserGuess > randomNum) 
-			{
-				
-				System.out.println("To high.  Try Again");
-			}
-			else
-			{
+    public static void main(String[] args) {
+        int minNum = 1;
+        int maxNum = 100;
+
+        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+
+        int randomNum = random.nextInt(maxNum - minNum + 1) + minNum;
+        int userGuess;
+        int attempts = 0;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("Guess a number between " + minNum + " and " + maxNum);
+
+        do {
+            System.out.print("Enter your guess: ");
+            userGuess = sc.nextInt();
+            attempts++;
+
+            if (userGuess < randomNum) {
+                System.out.println("Too low. Try again.");
+            } else if (userGuess > randomNum) {
+                System.out.println("Too high. Try again.");
+            } else {
                 System.out.println("Congratulations! You guessed the correct number in " + attempts + " attempts.");
+            }
+        } while (userGuess != randomNum);
 
-			}
-			
-			
-		}
-		while(UserGuess != randomNum);
-		
-		sc.close();
-		
-		
-		
-	}
-
+        sc.close();
+    }
 }
